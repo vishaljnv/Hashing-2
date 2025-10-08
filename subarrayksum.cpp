b8b93a9c051c82173f61cb2d334288e10c6a4d9b
Vishal Yarabandi <33473675+vishaljnv@users.noreply.github.com>
2025-10-08 08:06:06 +0530

    Create subarrayksum.cpp

---
+++ subarrayksum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) 
+    {
+        unordered_map<int, int> rSumCount;
+        int rSum = 0, result = 0;
+        rSumCount[0] = 1; //Edge case if sub-array starts at index 0
+
+        for (int i = 0; i < nums.size(); i++) {
+            rSum += nums[i];
+
+            if (rSumCount.contains(rSum - k)) {
+                result += rSumCount[rSum - k];
+            }
+
+            if (rSumCount.contains(rSum)) {
+                rSumCount[rSum] += 1;
+            } else {
+                rSumCount[rSum] = 1;
+            }
+        }
+
+        return result;    
+    }
+};
